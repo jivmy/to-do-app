2dc336dfc321a65b99c3dcaeb7e332591256e67c
Jimmy Shan <jimmy@jshan.ca>
2024-09-23 17:06:31 -0700

    toggle checklist

--- src/Task.tsx
+++ src/Task.tsx
@@ -1,33 +1,66 @@
-import { useState } from 'react';
+import React, { useState } from 'react'; 
 
-const Task = () => {
+const Tasks: React.FC = () => {
 
-    const [task, setTask] = useState<string>('Hello')
-    const [tasks, setTasks] = useState<string[]>([])
-
-    const handleDisplayTask = () => {
-      if (task.trim() !== '') {
-      setTasks([... tasks, task])
-      setTask('')
+    interface Task {
+        name: string;
+        cross: boolean;
     }
-  }
-    
-    return  (
-        <div>
-          <input
-            type = "text" 
-            value = {task}
-            onChange={(e) => setTask(e.target.value)}
-            placeholder = "Enter a task"
-          />
-          <button onClick={handleDisplayTask}>Add</button>
 
-          <ul>{tasks.map((t, index) => (
-            <li key={index}>{t}</li>
-          ))}
-          </ul>
+    const [task, setTask] = useState<Task>({name: '', cross: false});
+    const [tasks, setTasks] = useState<Task[]>([]);
+
+    const handleAddTask = () => {
+        if (task.name.trim() !== '') {
+            setTasks([...tasks, task]);
+            setTask({...task, name: ''});
+        }
+    }
+
+    const deleteTask = (i: number) => {
+        setTasks(tasks.filter((_, index) => index !== i));
+    }
+
+    const clearTask = () => {
+        setTasks([]);
+    }
+
+    const crossText = (index: number) => {
+        setTasks(tasks.map((t, i) => {
+            if (i === index) {
+                return {
+                    ...t,
+                    cross: !t.cross
+                }
+            }
+            return t;
+        }
+        ))
+    } 
+
+    return (
+        <div>
+            <input
+                type="text"
+                value={task.name}
+                onChange={(e) => setTask({...task, name: e.target.value})}
+                placeholder="Enter a task" 
+            />
+            <button onClick={handleAddTask}>Add task</button>
+            <button onClick={clearTask}>Clear task</button>
+            <ul>
+                {tasks.map((t, index) => (
+                    <li key={index}>
+                        <input type="checkbox" onChange={() => crossText(index)}/>
+                        <span style={{textDecoration: t.cross ? 'line-through' : 'none'}}>
+                        {t.name}
+                        </span>
+                        <button onClick={() => deleteTask(index)}>X</button>
+                    </li>
+                ))}
+            </ul>
         </div>
-      );
+    );
 }
 
-export default Task;
+export default Tasks;
